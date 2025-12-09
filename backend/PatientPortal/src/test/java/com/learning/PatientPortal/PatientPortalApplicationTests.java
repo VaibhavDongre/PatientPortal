e055ead2aea3542688762783ed25738f68007014
Vaibhav Dongre <21vaibhavdongre@gmail.com>
2025-12-09 15:19:38 +0530

    Initial project structure

---
+++ backend/PatientPortal/src/test/java/com/learning/PatientPortal/PatientPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.PatientPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
