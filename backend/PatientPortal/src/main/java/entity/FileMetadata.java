78c0252fc34bb7a000f97f36c9050620419283cb
Vaibhav Dongre <21vaibhavdongre@gmail.com>
2025-12-09 16:47:22 +0530

    Database connectivity added

---
+++ backend/PatientPortal/src/main/java/entity/FileMetadata.java
@@ -0,0 +1,22 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "documents")
+@Data
+public class FileMetadata {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String filename;
+    private String contentType;
+    private Long size;
+    private String storagePath;
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+}
